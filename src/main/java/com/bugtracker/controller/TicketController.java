293beba97375dfedcc37be009f88a4c79bf3489a
Gaurav Kumar <gauravguru9837@gmail.com>
2026-02-07 23:18:39 +0530

    Added Status enum, service layer, delete & status update

--- src/main/java/com/bugtracker/controller/TicketController.java
+++ src/main/java/com/bugtracker/controller/TicketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tickets")
+@RequestMapping("/api/tickets")
 @CrossOrigin
 public class TicketController {
 
